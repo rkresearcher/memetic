a684632f39879e60a5d75a26f1ae9e519e125325
RAHUL KUMAR <49233204+rkresearcher@users.noreply.github.com>
2020-07-07 18:16:38 +0530

    Add files via upload

--- code/local_search.py
+++ code/local_search.py
@@ -1,53 +1,45 @@
 from sklearn.neighbors import NearestNeighbors as nn
-from sklearn.neighbors import KNeighborsClassifier as knn
 import numpy as np
 import scipy.signal as ss
 
 class local_search():
-	def __init__(self,R,S,tau):
+	def __init__(self,R,S,tau,p):
 		self.R = R
 		self.S = S
 		self.tau =tau
-#		self.p = p
+		self.p = p
 	def SOR(self):
 		'''
 		single object replacement
 
 		'''
-		#for k in range(size(self.tau)):
-		#for k in p:
-		c = self.tau
-		Prob = permutationanother(self.tau)
-		for j in Prob:
-			for i in range(len(c)-1):
-
-				tau_c = self.tau
-#				x1 = nn(n_neighbors =2, algorithm = 'ball_tree').fit(self.R)
-#				distance, x = x1.kneighbors(self.R)
-#				x = x1.fit(self.R)
-#				[n,m] = self.tau.shape
-#				f_best = np.zeros([n,m])
-				f_best = 0
-				for e in self.R[i]:
-					tau_c[i] = e[i]
-					f_c = fitness(tau_c,self.S) #evalute the fitness: function calling fitness
-					
-					if f_c.all()> f_best:
-						x_best = e
-						f_best = f(tau_c)
-				tau_c[i] = x_best
-				c = c.union(tau_c)
+		for k in range(size(self.tau)):
+			c = self.tau[k]
+			Prob = tau_prob(tau)
+			for j in prob:
+				for i in range(n):
+					tau_c = tau
+					x1 = nn(n_neighbors =2, algorithm = 'ball_tree').fit(R)
+					distance, x = x1.kneighbors(R)
+					f_best = 0
+					for e in x:
+						tau_c[i] = e
+						#evalute the fitness: function calling fitness
+						if f(tau_c)> f_best:
+							x_best = e
+							f_best = f(tau_c)
+					tau_c[i] = x_best
+					c = c.union(tau_c)
 		return c
 
 def fitness(t_c, S):
 	n = len(S)
-	t = 2/(n*(n-1))
+	t = 2/(n(n-1))
 	phi_inner = 0
 	phi_outer = 0
-	theta = 50
 	for i in range(1,n+1):
 		for j in range(i+1,n+1):
-			p = trap(theta)*mu_pair(t_c,S,0,t_c,S,0) # mu_pair function calling
+			p = trap(theta)*mu_pair() # mu_pair function calling
 			phi_inner  = phi_inner+p
 		phi_outer = phi_outer+phi_inner	
 	return t*phi_outer
@@ -55,37 +47,36 @@ def fitness(t_c, S):
 
 def mu_pair(o_i,o_j,theta_ij,o_i_dash,o_j_dash,theta_ij_dask):
 	beta = 0.5
-	force_0_ab,force_2_ab = force(o_i,o_j)
-	force_0_ab_dash,force_2_ab_dash = force(o_i_dash,o_j_dash)
+	force_0_ab = force()
+	force_2_ab_dash = force()
 
 
-	uc_0 = ss.correlate(force_0_ab,force_0_ab_dash)
+	uc_0 = ss.corelate(force_0_ab,force_0_ab_das)
 	uc_2 = ss.correlate(force_2_ab,force_2_ab_dash)
 
 	return 0.5*uc_0 + 0.5*uc_2
 
-def force(A,B):
+def force():
 	const_force = 10
-#	print (type(A))
-	[n,m] = A.shape
-	[n1,m1] = B.shape
+	[n,m] = size(A)
+	[n1,m1] = size(B)
 	mass_A = n*m
 	mass_B = n1*m1
-	f_0 = const_force
 	for i in range(10):
 		'''
 		constant force change as the change in the thickness between the object
 		'''
 		pass
 	F_2 = []
-	for j in A: # verify or change range(10), by the number of elements in the for matching
-		for k in B:
+	for j in arrary_A: # verify or change range(10), by the number of elements in the for matching
+		for k in array_B:
+			[n,m] = size(A)
+			[n1,m1] = size(B)
 			mass_A = n*m
 			mass_B = n1*m1
-#			d = ((x1-x2)**2+(y1-y2)**2)**(1/2)        # use distance formula
-			d = 10
+			d = ((x1-x2)**2+(y1-y2)**2)**(1/2)        # use distance formula
 			f_2 = mass_A*mass_B/(d**2)
-		F_2.append(f_2)
+		F_2.apped(f_2)
 	return F_2, f_0
 
 
@@ -122,10 +113,8 @@ def permutationanother (list):
 
 	from itertools import permutations
 	perm = permutations(list)
-#	for t in perm:
-#		pass
-#        	print(t)
-	return perm
+	for t in perm:
+        	print (t)
 
 def permutation(list):
 	'''
@@ -144,7 +133,7 @@ def permutation(list):
 			l.append([m] +p)
 	return l
 
-#data = list('123')
+data = list('123')
 #for q in permutation (data):
 #	print (q)
-#data = permutationanother(data)
+data = permutationanother(data)
--- code/memtic.py
+++ code/memtic.py
@@ -1,56 +1,31 @@
-import cv2
 from local_search import *
 import random
-import scipy.io as io
-
-#def load(load):
-input = io.loadmat('pat_m_10.mat')
-refernce = cv2.imread('../fused_image_middle10.png',0)
-refernce = np.array(refernce)
-
 class memtic():
-	def __init__(self, input, reference, u=10, lam = 9, tau=7, u_elite = 7):
-		self.input = input
-		self.reference = reference
+	def __init__(self, Input,Reference, Parent, u=10, lam = 9, tau=7, u_elite = 7):
+		self.Input = Input
+		self.Reference = Reference
 		self.u = u
 		self.lam = lam
 		self.tau = tau
-		self.u_elite = 7
-		
-	def memtic_algo(self):
-		Input = self.input
-		Reference  = self.reference
-		t = 0
-		p1 = self.tau
-#		print (p1.type())
-#		exit()
-		p =  Input  # population
-#		print (p)
-#		exit()
-		img = []
-		while (t< len(p)):
-			for q in range(10):
-				for j in range(31):
-	
-					i = p[q][j]
-#					print (i)
-#					exit()
-					if i.all() > self.tau:
-						i = random.choice(p)      # replace with random individual
-					c1 = local_search(Input, Reference, i)
-					c = c1.SOR()
-					i = c
-					p_d = remove_duplicate(p)
-				while (len(p) <  u):
-					img.append(p(i))
-				t = t+1		
+		self.u_elite = u_elite
 
-		return img
+	def memtic_algo():
+		t = 0
+		p =    []  # population
+		while (t< len(P)):
+			for i in p:
+				if i > self.tau:
+					i = random.choice(p)      # replace with random individual
+				c = local_search.local_search.SOR(Input, Reference, tau, p)
+				i = c
+				p_d = remove_duplicate(p)
+			while (len(p) <  u):
+				p.append(p(i))
+			t = t+1
+
+		return p
 
 
 def remove_duplicate(x):
 	return list(dist.fromkeys(x))
 
-p = memtic(input['patches'],refernce)
-p1 = p.memtic_algo()
-print (p1)
